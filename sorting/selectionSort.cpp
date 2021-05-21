2af278d054b0048c94df08b2c76ab05da4abf511
PavanKumar <51289314+Gumparthypavankumar@users.noreply.github.com>
2021-05-21 18:33:23 +0530

    file in folder: Add selection sort in sorting

---
+++ sorting/selectionSort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// This sort in ascending order
+void selection_sort(int a[], int n){
+    for(int i=0;i<n;i++){
+        int minIndex = i; // Minimum index till this point we always assume the curr position as minimum
+        for(int j=i+1;j<n;j++){
+            if(a[minIndex] > a[j]){
+                minIndex = j;
+            }
+        }
+        swap(a[i], a[minIndex]);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    selection_sort(a,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
